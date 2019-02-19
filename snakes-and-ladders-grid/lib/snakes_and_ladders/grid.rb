838f39531e359dd9924771243d409fabf3b873f7
baxter2 <baxcallan@gmail.com>
2019-02-19 12:28:03 +1000

    upddate

--- snakes-and-ladders-grid/lib/snakes_and_ladders/grid.rb
+++ snakes-and-ladders-grid/lib/snakes_and_ladders/grid.rb
@@ -1,5 +1,7 @@
 module SnakesAndLadders
   class Grid
+    attr_reader :size, :tile_mappings, :default_tile
+
     def initialize(size:, tile_mappings: [], default_tile: :Cell)
       @size = size
       @tile_mappings = tile_mappings
@@ -14,6 +16,21 @@ module SnakesAndLadders
       hash[4] = Portal.new(location: 4, destination: 2)
       hash
     end
+
+    def build_v2
+      (1..size).each_with_object({}) { |index, grid| grid[index] = tile_at(index) }
+    end
+
+    private
+
+      def tile_at(index)
+        if mapping = tile_mappings.detect { |tile_map| tile_map[:location].equal?(index) }
+          klass = mapping.delete(:class)
+          SnakesAndLadders.const_get(klass).new(mapping)
+        else
+          SnakesAndLadders.const_get(default_tile).new(location: index)
+        end
+      end
 
   end 
 
