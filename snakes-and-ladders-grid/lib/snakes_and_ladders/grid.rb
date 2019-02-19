0879494c073774fd9d2891341f5b67a42d367f78
baxter2 <baxcallan@gmail.com>
2019-02-19 12:19:45 +1000

    update

--- snakes-and-ladders-grid/lib/snakes_and_ladders/grid.rb
+++ snakes-and-ladders-grid/lib/snakes_and_ladders/grid.rb
@@ -1,7 +1,36 @@
 module SnakesAndLadders
   class Grid
-    def initialize()
+    def initialize(size:, tile_mappings: [], default_tile: :Cell)
+      @size = size
+      @tile_mappings = tile_mappings
+      @default_tile = default_tile
+    end
 
+    def build_v1
+      hash = {}  
+      hash[1] = Portal.new(location: 1, destination: 3)
+      hash[2] = Cell.new(location: 2)
+      hash[3] = Cell.new(location: 3)
+      hash[4] = Portal.new(location: 4, destination: 2)
+      hash
+    end
+
+  end 
+
+  class Portal
+    attr_reader :location, :destination
+
+    def initialize(location: location, destination: destination)
+      @location = location
+      @destination = destination
+    end
+  end
+
+  class Cell
+    attr_reader :location
+
+    def initialize(location: location)
+      @location = location
     end
   end
 end
--- snakes-and-ladders-grid/spec/snakes_and_ladders/grid_spec.rb
+++ snakes-and-ladders-grid/spec/snakes_and_ladders/grid_spec.rb
@@ -10,8 +10,17 @@ module SnakesAndLadders
         ]
       end
 
-      it "builds a grid from mappings" do
-        grid = Grid.new(size: 4, tile_mappings: tile_mappings).build
+      it "VERSION 1 ruturns a Hash object" do
+        tile_mappings = [{:class=>:Portal, :location=>1, :destination=>3},  {:class=>:Portal, :location=>4, :destination=>2}]
+
+        grid = Grid.new(size: 100, tile_mappings: tile_mappings, default_tile: :Cell) 
+        grid_built = grid.build_v1
+
+        expect(grid_built).to be_a Hash
+      end
+
+      it "VERSION 1 builds a grid from tile_mappings" do
+        grid = Grid.new(size: 4, tile_mappings: tile_mappings).build_v1
 
         expect(grid[1]).to be_a Portal
         expect(grid[2]).to be_a Cell
@@ -27,13 +36,38 @@ module SnakesAndLadders
         expect(grid[4].destination).to eq 2
       end
 
-      it "ruturns a Hash object" do
-        mappings = [{:class=>:Portal, :location=>1, :destination=>3}, {:class=>:Portal, :location=>4, :destination=>2}]
+      it "VERSION 2 ruturns a Hash object" do
+        tile_mappings = [{:class=>:Portal, :location=>1, :destination=>3},  {:class=>:Portal, :location=>4, :destination=>2}]
 
-        grid = Grid.new(size: 100, tile_mappings: mappings, default_tile: :Cell) 
+        grid = Grid.new(size: 100, tile_mappings: tile_mappings, default_tile: :Cell) 
+        grid_built = grid.build_v2
 
-        expect(grid.build).to be_a Hash
+        expect(grid_built).to be_a Hash
       end
+
+      it "VERSION 2 builds a grid from tile_mappings" do
+        grid = Grid.new(size: 4, tile_mappings: tile_mappings).build_v2
+
+        expect(grid[1]).to be_a Portal
+        expect(grid[2]).to be_a Cell
+        expect(grid[3]).to be_a Cell
+        expect(grid[4]).to be_a Portal
+
+        expect(grid[1].location).to eq 1
+        expect(grid[2].location).to eq 2
+        expect(grid[3].location).to eq 3
+        expect(grid[4].location).to eq 4
+
+        expect(grid[1].destination).to eq 3
+        expect(grid[4].destination).to eq 2
+      end
+
     end
   end
 end
+
+# >> SnakesAndLadders::Grid.new(size: 100, tile_mappings: mappings, default_tile: :Cell).build
+# => {1=> SnakesAndLadders::Portal0x0 @destination=3, @location=1, @players=[]>, 
+#    {2=> SnakesAndLadders::Cell                      @location=2, @players=[]>, 
+#     3=> SnakesAndLadders::Cell                      @location=3, @players=[]>, 
+#     4=> SnakesAndLadders::Portal0x0 @destination=2, @location=4, @players=[]>}
